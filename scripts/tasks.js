67cf895adfda4c3de9a82b776f67599741eb9e51
TsaganBelg <33128196+TsaganBelg@users.noreply.github.com>
2017-12-14 20:39:33 +0300

    Update tasks.js
    homework

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,53 +1,100 @@
-// 1) Написать функцию getFieldValues, которая будет принимать на вход массив объектов, 
-// а возвращать – массив значений одного из полей (отсортированных в порядке возрастания):
-// ------------------------
+// task 1
 
 let usersData = [
 	{ 'user' : 'Alex', 'password' : 'MyNameIsAlex' },
-	{ 'user' : 'Bob', 'password' : 'MyNAmeIsBob' }
+	{ 'user' : 'Bob', 'password' : 'MyNameIsBob' }
 ];
-console.log(getFieldValues(usersData, 'user')); // --> ['Alex', 'Bob']
 
-// ------------------------
+function getFieldValues(usersData, 'user') {
+		let result = [];
+		usersData.forEach(function(name, i, usersData) {
+			result.push(name['user']);
+		});
+		return result.sort();
+}
+console.log(getFieldValues(usersData, 'user'));
 
-
-// 2) Написать функцию, фильтрующую массив с использованием предиката:
-// ------------------------
+// task 2
 
 let numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
-function isEven(x) {/* Проверка на чётность */}
-console.log(filter(numbers, isEven)); // --> [2, 8, 34]
+function isEven(x) {if (x % 2==0) return x;}
+function filter(X, func1){return X.filter(func1);}
+console.log(filter(numbers, isEven)); 
 
-// ------------------------
-
-
-// 3) Даны 2 строки со словами (без знаков препинания), 
-// вывести те слова (по одному разу), которые встречаются в обоих строках
-// ------------------------
+// task 3 
 
 var firstLongString = 'Load up on guns and bring your friends it is fun to lose and to pretend';
 var secondLongString = 'She is over bored and self assured oh no I know a dirty word';
-console.log(findSimilarWords(firstLongString, secondLongString)); // --> ['and', 'is'];
+var firstarr = str.split(' ');
+var secondarr = str.split(' ');
+function findSimilarWords(firstarr, secondarr){
+  var result = [];
 
-// ------------------------
+ nextInput:
+    for (var i = 0; i < firstarr.length; i++) {
+      var str = firstarr[i]; 
+      for (var j = 0; j < second.length; j++) { 
+        if (result[j] == str) continue nextInput;
+      }
+      result.push(str);
+    }
 
+  return result;
+}
+console.log(findSimilarWords(firstarr, secondarr));
 
-
-// 4) Дан IP-адрес (строка) и маска подсети (десятичное число). Написать функцию, которая будет валидировать
-// IP-адрес (4 октета, <= 255), а затем выводить сетевой и широковещательный адреса:
-// ------------------------
+// task 4
 
 var IpAddress = '10.223.98.2';
 var subnetMask = 28;
-console.log(generateBroadcastAndNetworsAddresses(IpAddress, subnetMask)); // Broadcast - 10.223.98.15, Network - 10.223.98.0
+function generateBroadcastAndNetworsAddresses(IpAddress, bitmask) {
+ 	if (typeof IpAddress != 'string')
+ 		return 'IpAddress ' + IpAddress + ' must be a string';
+ 
+ 	var arr = IpAddress.split('.');
+ 	if (arr.length != 4)
+ 		return 'IpAddress ' + IpAddress + ' must consist of 4 octets';
+ 
+ 	if (arr.some(function(num) {
+ 		return isNaN(parseInt(num)) || num < 0 || num > 255;
+ 	})) return 'IpAddress ' + IpAddress + ' must consist of numbers from 0 to 255';
+ 
+ 	var IpAddressFull = 0;
+ 	for (var i = 0; i < 4; i++) {
+ 		IpAddressFull += arr[i] << (3 - i) * 8;
+ 	}
+ 
+ 	var subnetMaskFull = Math.pow(2, 32) - Math.pow(2, 32 - bitmask);
+ 
+ 	var networkFull   = IpAddressFull & subnetMaskFull;
+ 	var broadcastFull = networkFull | ~subnetMaskFull;
+ 
+ 	var network   = IPv4_intA_to_dotquadA(networkFull);
+ 	var broadcast = IPv4_intA_to_dotquadA(broadcastFull);
+ 
+ 	return 'Broadcast - ' + broadcast + ', Network - ' + network;
+ }
+ function IPv4_intA_to_dotquadA(num) {
+     var byte1 = (num >>> 24);
+     var byte2 = (num >>> 16) & 255;
+     var byte3 = (num >>>  8) & 255;
+     var byte4 = num & 255;
+     return byte1 + '.' + byte2 + '.' + byte3 + '.' + byte4;
+ }
+console.log(generateBroadcastAndNetworsAddresses(IpAddress, subnetMask)); 
 
-// ------------------------
-
-
-// 5) Соединить все массивы в один, не допуская повторения элементов (порядок не важен):
-// ------------------------
+// task 5
 
 var totalMessArray = [['a', 1, true], [true, 99, 'aa', undefined], ['1']];
-console.log(makeItClean(totalMessArray)); // --> ['a', 'aa', 1, '1', undefined, true];
-
-// ------------------------
+console.log(makeItClean(totalMessArray));
+function makeItClean(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result = result.concat(arr[i]);
+    }
+    result = result.filter(function(element, e) {
+        return result.indexOf(element) == e;
+    });
+    return result;
+}
+console.log(makeItClean(totalMessArray));
